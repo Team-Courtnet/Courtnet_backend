b75da549ccdff384a56f692b33fbdfd00b2eed5d
jay91537 <veuxft@gmail.com>
2024-10-15 23:40:36 +0900

    chore : first commit

---
+++ src/main/java/courtnet/CourtnetApplication.java
@@ -0,0 +1,13 @@
+package courtnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourtnetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/courtnet/CourtnetApplicationTests.java
@@ -0,0 +1,13 @@
+package courtnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
