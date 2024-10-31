4aaa85881d1817bd7d3ed9369d0ce6c8136f0a51
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-10-31 14:13:29 +0900

    config: 데이터베이스 쿼리 로깅을 위한 프록시 객체 추가

---
+++ src/main/java/courtnet/common/log/model/LoggingForm.java
@@ -0,0 +1,20 @@
+package courtnet.common.log.model;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LoggingForm {
+
+    private final String apiUrl;
+
+    private final String apiMethod;
+
+    private Long queryCounts = 0L;
+
+    private Long queryTime = 0L;
+
+    public void queryExecuted(final Long queryTime) {
+        queryCounts++;
+        this.queryTime += queryTime;
+    }
+}
